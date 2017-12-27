ce6ff043a1dcc8ad616ba6ede432409f64d2d054
William Dvorak <wdvorak@Williams-MacBook.local>
2017-12-26 23:16:42 -0500

    transfer

--- commands.sh
+++ commands.sh
@@ -4,3 +4,7 @@ docker rm -f $(docker ps -aq); docker run -t latest
 
 docker build -f Dockerfile_slave -t latest .
 docker rm -f $(docker ps -aq); docker run -t latest
+
+
+docker build -f Dockerfile_analytics -t latest .
+docker rm -f $(docker ps -aq); docker run -t latest
