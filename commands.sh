8326c4169bf003605af4be613d4452b2b6d1e471
William Dvorak <wdvorak@Williams-MacBook.local>
2017-12-28 18:57:22 -0500

    transfer

--- commands.sh
+++ commands.sh
@@ -3,8 +3,8 @@ docker rm -f $(docker ps -aq); docker run -t latest
 
 
 docker build -f Dockerfile_slave -t latest .
-docker rm -f $(docker ps -aq); docker run -t latest
+docker run -t latest
 
 
 docker build -f Dockerfile_analytics -t latest .
-docker rm -f $(docker ps -aq); docker run -t latest
+docker run -t latest
--- source/sel.py
+++ source/sel.py
@@ -21,10 +21,10 @@ delay = .5
 pageDelay = 5
 
 checkboxPositives = ['remove', 'stop', 'unsub', 'off', 'opt out']
-buttonPositives = ['remove', 'stop', 'unsub', 'go', 'submit', 'click', 'opt out', 'yes', 'update', 'don\'t send me any', 'don\'t send any', 'stop sending', 'confirm']
+buttonPositives = ['remove', 'stop', 'unsub', 'go', 'submit', 'click', 'opt out', 'yes', 'update', 'don\'t send me any', 'don\'t send any', 'stop sending', 'confirm', 'do not email', 'block all emails']
 radioPositives = ['all', 'none', 'complete']
 confirmPositives = ['unsubscribed', 'success', 'thank you', 'updated', 'have removed', 'request has been processed', 'request processed', 'unsubscribe confirmation']
-shortConfirmPositives = ['successfully unsubscribed', 'confirmed unsubscribed', 'now unsubscribed', 'unsubscribe successful', 'successfully removed', 'unsubscribe confirmation']
+shortConfirmPositives = ['successfully unsubscribed', 'confirmed unsubscribed', 'now unsubscribed', 'unsubscribe successful', 'successfully removed', 'unsubscribe confirmation','unsubscribed successfully']
 
 js_code = "return document.getElementsByTagName('html')[0].innerHTML;"
   
