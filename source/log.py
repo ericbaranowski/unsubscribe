c89d4acd42306dcb3f6f0d4037e1f19797788f4c
William Dvorak <wdvorak@Williams-MacBook.local>
2018-01-04 17:49:33 -0500

    transfer

--- source/log.py
+++ source/log.py
@@ -2,30 +2,31 @@ from google.cloud import logging
 
 import sys
 
-cloudLog = False
+cloudLog = True
+logger = None
 
 
 if cloudLog:
   bucket = 'main'
-  logging_client = logging.Client('hosting-2718')
-  logger = logging_client.logger(bucket)
-
-
-def log(*entry):
-  e = entry
   try:
-    e = str(entry)
+    logging_client = logging.Client('hosting-2718')
+    logger = logging_client.logger(bucket)
   except:
-    e = entry
-  try:
-    if cloudLog:
-      logger.log_text(e)
-    print >> sys.stderr, str(e.decode('utf-8', 'replace'))
-  except Exception as e:
-    print e
+    pass
+
+def log(entry, bucket='main', severity='INFO'):
+  if logger:
+    try:
+      logger.log_text(str(entry), severity=severity)
+    except Exception as e:
+      print >> sys.stderr, 'failed logging', str(e)
+  else:
+    try:
+      print >> sys.stderr, str(str(entry).encode('utf-8', 'replace'))
+    except Exception as e:
+      print >> sys.stderr, 'failed printing', str(e)
 
 def debug(entry, bucket='main'):
-  return
   log(entry, bucket, 'DEBUG')
 
 def info(entry, bucket='main'):
