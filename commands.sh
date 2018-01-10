91b6d641dc32db24b808df00df6f78d44456b260
William Dvorak <wdvorak@Williams-MacBook.local>
2018-01-10 02:51:32 -0500

    transfer

--- commands.sh
+++ commands.sh
@@ -1,10 +1,26 @@
 cd unsubscribe; git pull; cd ..
 
 docker build -t master .
-docker rm -f $(docker ps -aq); docker run -t master &
+docker rm -f $(docker ps -aq); docker run --restart always -t master   &
 
 docker build -f Dockerfile_slave -t slave .
-docker run -t slave &
+docker run --restart always -t slave  &
 
 docker build -f Dockerfile_analytics -t analytics .
 docker run -t analytics &
+
+
+vim /etc/rc.local
+cd /home/wdvorak; sudo docker build -t master . 
+cd /home/wdvorak; sudo docker rm -f $(docker ps -aq)
+cd /home/wdvorak; sudo docker build -f Dockerfile_slave -t slave .
+cd /home/wdvorak; sudo docker run --restart always -t master &
+cd /home/wdvorak; sudo docker run --restart always -t slave &
+
+export VISUAL=vim
+crontab -e
+
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+* */2 * * * sudo gcloud compute --project "hosting-2718" instances stop --zone "us-east1-d" "unsub"; sudo gcloud compute --project "hosting-2718" instances start --zone "us-east1-d" "unsub"
--- source/log.py
+++ source/log.py
@@ -10,7 +10,7 @@ tid = 'tiddefault'
 if cloudLog:
   bucket = 'main'
   try:
-    logging_client = logging.Client('consulting-2718')
+    logging_client = logging.Client('hosting-2718')
     logger = logging_client.logger(bucket)
   except:
     pass
