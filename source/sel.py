f4c1c3b527f15c479b31ed621ba26a2540875f51
William Dvorak <wdvorak@Williams-MacBook.local>
2018-05-04 17:39:28 -0400

    transfer

--- source/sel.py
+++ source/sel.py
@@ -16,27 +16,29 @@ confirmPositives = ['unsubscribed', 'success', 'thank you', 'updated', 'have rem
 js_code = "return document.getElementsByTagName('html')[0].innerHTML;"
 
 class Timeout():
-    """Timeout class using ALARM signal."""
-    class Timeout(Exception):
-        pass
- 
-    def __init__(self, sec):
-        self.sec = sec
- 
-    def __enter__(self):
-        signal.signal(signal.SIGALRM, self.raise_timeout)
-        signal.alarm(self.sec)
- 
-    def __exit__(self, *args):
-        signal.alarm(0)    # disable alarm
- 
-    def raise_timeout(self, *args):
-        raise Timeout.Timeout()
+  """Timeout class using ALARM signal."""
+  class Timeout(Exception):
+    pass
+
+  def __init__(self, sec):
+    self.sec = sec
+
+  def __enter__(self):
+    signal.signal(signal.SIGALRM, self.raise_timeout)
+    signal.alarm(self.sec)
+
+  def __exit__(self, *args):
+    signal.alarm(0)    # disable alarm
+
+  def raise_timeout(self, *args):
+    raise Timeout.Timeout()
 
 def browserGetPage(browser,url): 
   try:
     with Timeout(pageTimeout):
+      log.info('start get')
       browser.get(url)
+      log.info('end get')
   except Exception as e:
     log.warn(e)
     log.warn('timed out on' + url)
